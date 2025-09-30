28c08d4034c4cf80941de59de8843f86ccd22d62
“RamirezLuisManuel” <luismanuelr245@gmail.com>
2025-09-30 00:49:22 -0600

    Subir back configurado

---
+++ server/build/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    app;
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.config();
+        this.routes();
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+    routes() { }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
+new Server(); // ejecuta la clase y devuelve un objeto.
+console.log('WORKS!!!!!');
+//# sourceMappingURL=index.js.map
